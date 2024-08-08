5299dd77c9ca66bf82360638847ecbf7156c28be
Bertrand Juglas <bertrand.juglas@arthera.net>
2024-08-08 17:49:51 +0200

    set Gateway and RPC endpoints for Arthera

--- src/processor.ts
+++ src/processor.ts
@@ -9,8 +9,16 @@ import {
 import * as usdcAbi from './abi/usdc'
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        archive: 'https://v2.archive.subsquid.io/network/ethereum-mainnet',
+    .setGateway('https://v2.archive.subsquid.io/network/arthera-mainnet')
+    // Chain RPC endpoint is required for
+    //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
+    //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
+    .setRpcEndpoint({
+        // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
+        // https://docs.subsquid.io/deploy-squid/env-variables/
+        url: assertNotNull(process.env.RPC_ETH_HTTP, 'No RPC endpoint supplied'),
+        // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
+        rateLimit: 10
     })
     .addLog({
         topic0: [usdcAbi.events.Mint.topic],
